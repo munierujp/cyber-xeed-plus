e1e0172b0e43848afdabeece6f877d1a1ae0fbd6
Munieru <20086673+munierujp@users.noreply.github.com>
2019-03-10 16:11:02 +0900

    Fix indent

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -3,37 +3,36 @@ import Storage from './modules/Storage'
 import defaultConfig from './modules/default_config'
 
 document.addEventListener('DOMContentLoaded', () => {
-  Storage.fetch(defaultConfig)
-    .then(config => {
-      /* eslint-disable no-new */
-      new Vue({
-        el: '#app',
-        data: () => ({
-          status: '',
-          ログイン画面で自動ログイン: config.ログイン画面で自動ログイン,
-          勤務データ確認画面に今月ボタンを追加: config.勤務データ確認画面に今月ボタンを追加,
-          就業週報月報画面に今月ボタンを追加: config.就業週報月報画面に今月ボタンを追加,
-          就業週報画面に今月ボタンを追加: config.就業週報画面に今月ボタンを追加,
-          就業月報画面に今月ボタンを追加: config.就業月報画面に今月ボタンを追加,
-          就業年報画面に今年ボタンを追加: config.就業年報画面に今年ボタンを追加
-        }),
-        methods: {
-          onUpdate () {
-            this.status = ''
-          },
-          save () {
-            const config = {
-              ログイン画面で自動ログイン: this.ログイン画面で自動ログイン,
-              勤務データ確認画面に今月ボタンを追加: this.勤務データ確認画面に今月ボタンを追加,
-              就業週報月報画面に今月ボタンを追加: this.就業週報月報画面に今月ボタンを追加,
-              就業週報画面に今月ボタンを追加: this.就業週報画面に今月ボタンを追加,
-              就業月報画面に今月ボタンを追加: this.就業月報画面に今月ボタンを追加,
-              就業年報画面に今年ボタンを追加: this.就業年報画面に今年ボタンを追加
-            }
-            Storage.set(config)
-            this.status = '保存しました。'
+  Storage.fetch(defaultConfig).then(config => {
+    /* eslint-disable no-new */
+    new Vue({
+      el: '#app',
+      data: () => ({
+        status: '',
+        ログイン画面で自動ログイン: config.ログイン画面で自動ログイン,
+        勤務データ確認画面に今月ボタンを追加: config.勤務データ確認画面に今月ボタンを追加,
+        就業週報月報画面に今月ボタンを追加: config.就業週報月報画面に今月ボタンを追加,
+        就業週報画面に今月ボタンを追加: config.就業週報画面に今月ボタンを追加,
+        就業月報画面に今月ボタンを追加: config.就業月報画面に今月ボタンを追加,
+        就業年報画面に今年ボタンを追加: config.就業年報画面に今年ボタンを追加
+      }),
+      methods: {
+        onUpdate () {
+          this.status = ''
+        },
+        save () {
+          const config = {
+            ログイン画面で自動ログイン: this.ログイン画面で自動ログイン,
+            勤務データ確認画面に今月ボタンを追加: this.勤務データ確認画面に今月ボタンを追加,
+            就業週報月報画面に今月ボタンを追加: this.就業週報月報画面に今月ボタンを追加,
+            就業週報画面に今月ボタンを追加: this.就業週報画面に今月ボタンを追加,
+            就業月報画面に今月ボタンを追加: this.就業月報画面に今月ボタンを追加,
+            就業年報画面に今年ボタンを追加: this.就業年報画面に今年ボタンを追加
           }
+          Storage.set(config)
+          this.status = '保存しました。'
         }
-      })
+      }
     })
+  })
 })
