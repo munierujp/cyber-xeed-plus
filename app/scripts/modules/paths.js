dcc7d2979a2feb83d15d85f48998336da153a859
Munieru <20086673+munierujp@users.noreply.github.com>
2019-03-10 10:03:19 +0900

    Add 就業年報画面

--- app/scripts/modules/paths.js
+++ app/scripts/modules/paths.js
@@ -3,5 +3,6 @@ export default {
   勤務データ確認画面: '/cyberx/page/Xyw1100g.asp',
   就業週報月報画面: '/cyberx/page/Xaw1500.asp',
   就業週報画面: '/cyberx/page/Xaw1800.asp',
-  就業月報画面: '/cyberx/page/Xyw3300.asp'
+  就業月報画面: '/cyberx/page/Xyw3300.asp',
+  就業年報画面: '/cyberx/page/Xaw4000.asp'
 }
