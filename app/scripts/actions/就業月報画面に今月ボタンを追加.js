0f7ae670bddd8470a88e6edc1ff099b2359ce526
Munieru <20086673+munierujp@users.noreply.github.com>
2019-03-10 09:40:12 +0900

    Add "就業月報画面に今月ボタンを追加" action (#11)
    * Add 就業月報画面
    
    * Add 就業月報画面に今月ボタンを追加.js
    
    * Add 就業月報画面に今月ボタンを追加
    
    * Add 就業月報画面に今月ボタンを追加

---
+++ app/scripts/actions/就業月報画面に今月ボタンを追加.js
@@ -0,0 +1,44 @@
+import getNow from '../modules/getNow'
+
+export default function () {
+  const searchButton = getSearchButtonElement()
+  const currentMonthButtonCell = createCurrentMonthButtonCellElement(searchButton)
+  const searchButtonCell = searchButton.parentElement
+  searchButtonCell.parentNode.insertBefore(currentMonthButtonCell, searchButtonCell)
+}
+
+function getSearchButtonElement () {
+  return document.getElementById('cmdKensaku')
+}
+
+function createCurrentMonthButtonCellElement (searchButton) {
+  const cell = document.createElement('td')
+  cell.setAttribute('nowrap', true)
+  const button = createCurrentMonthButtonElement(searchButton)
+  cell.append(button)
+  return cell
+}
+
+function createCurrentMonthButtonElement (searchButton) {
+  const button = document.createElement('input')
+  button.setAttribute('type', 'button')
+  button.setAttribute('value', '今月')
+  button.addEventListener('click', () => {
+    const options = getOptionElements()
+    const month = createCurrentMonth()
+    options.forEach(option => {
+      option.selected = option.value === month
+    })
+    searchButton.click()
+  })
+  return button
+}
+
+function getOptionElements () {
+  const select = document.getElementById('cboYM')
+  return Array.from(select.querySelectorAll('option'))
+}
+
+function createCurrentMonth () {
+  return getNow().toFormat('yyyyMM')
+}
--- app/scripts/modules/paths.js
+++ app/scripts/modules/paths.js
@@ -1,5 +1,6 @@
 export default {
   ログイン画面: '/cyberx/login.asp',
   勤務データ確認画面: '/cyberx/page/Xyw1100g.asp',
-  就業週報画面: '/cyberx/page/Xaw1800.asp'
+  就業週報画面: '/cyberx/page/Xaw1800.asp',
+  就業月報画面: '/cyberx/page/Xyw3300.asp'
 }
