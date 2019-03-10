c99a3881461a2176549c3c165a318b1a5ed4a212
Munieru <20086673+munierujp@users.noreply.github.com>
2019-03-10 09:01:17 +0900

    Add 勤務データ確認画面に今月ボタンを追加.js

---
+++ app/scripts/actions/勤務データ確認画面に今月ボタンを追加.js
@@ -0,0 +1,53 @@
+import getStartOfMonth from '../modules/getStartOfMonth'
+import getNow from '../modules/getNow'
+
+export default function () {
+  const searchButton = getSearchButtonElement()
+  const currentMonthButtonCell = createCurrentMonthButtonCellElement(searchButton)
+  const searchButtonCell = searchButton.parentElement
+  searchButtonCell.parentNode.insertBefore(currentMonthButtonCell, searchButtonCell)
+}
+
+function getSearchButtonElement () {
+  return document.getElementsByName('srchbutton')[0]
+}
+
+function createCurrentMonthButtonCellElement (searchButton) {
+  const cell = document.createElement('td')
+  cell.setAttribute('nowrap', true)
+  const button = createCurrentMonthButtonElement(searchButton)
+  cell.append(button)
+  return cell
+}
+
+function createCurrentMonthButtonElement (searchButton) {
+  const button = document.createElement('input')
+  button.setAttribute('type', 'button')
+  button.setAttribute('value', '今月')
+  button.addEventListener('click', () => {
+    const startDateForm = getStartDateFormElement()
+    const startDate = createStartDate()
+    startDateForm.value = startDate
+    const endDateForm = getEndDateFormElement()
+    const endDate = createEndDate()
+    endDateForm.value = endDate
+    searchButton.click()
+  })
+  return button
+}
+
+function getStartDateFormElement () {
+  return document.getElementsByName('StartYMD')[0]
+}
+
+function createStartDate () {
+  return getStartOfMonth().toFormat('yyyyMMdd')
+}
+
+function getEndDateFormElement () {
+  return document.getElementsByName('EndYMD')[0]
+}
+
+function createEndDate () {
+  return getNow().toFormat('yyyyMMdd')
+}
