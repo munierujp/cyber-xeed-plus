a808f4112104abfe463b0b7ae5fdec1840608fb5
Munieru <20086673+munierujp@users.noreply.github.com>
2019-03-10 10:30:48 +0900

    Add "就業年報画面に今年ボタンを追加" action (#17)
    * Add 就業年報画面
    
    * Add getStartOfYear.js
    
    * Add 就業年報画面に今年ボタンを追加.js
    
    * Add 就業年報画面に今年ボタンを追加
    
    * Add 就業年報画面に今年ボタンを追加

---
+++ app/scripts/actions/就業年報画面に今年ボタンを追加.js
@@ -0,0 +1,53 @@
+import getStartOfYear from '../modules/getStartOfYear'
+import getNow from '../modules/getNow'
+
+export default function () {
+  const searchButton = getSearchButtonElement()
+  const currentMonthButtonCell = createCurrentMonthButtonCellElement(searchButton)
+  const searchButtonCell = searchButton.parentElement
+  searchButtonCell.parentNode.insertBefore(currentMonthButtonCell, searchButtonCell)
+}
+
+function getSearchButtonElement () {
+  return document.getElementById('cmdSearch')
+}
+
+function createCurrentMonthButtonCellElement (searchButton) {
+  const cell = document.createElement('td')
+  cell.setAttribute('valign', 'bottom')
+  const button = createCurrentMonthButtonElement(searchButton)
+  cell.append(button)
+  return cell
+}
+
+function createCurrentMonthButtonElement (searchButton) {
+  const button = document.createElement('input')
+  button.setAttribute('type', 'button')
+  button.setAttribute('value', '今年')
+  button.addEventListener('click', () => {
+    const startMonthForm = getStartMonthFormElement()
+    const startMonth = createStartMonth()
+    startMonthForm.value = startMonth
+    const endMonthForm = getEndDMonthFormElement()
+    const endMonth = createEndDMonth()
+    endMonthForm.value = endMonth
+    searchButton.click()
+  })
+  return button
+}
+
+function getStartMonthFormElement () {
+  return document.getElementById('cboYM1')
+}
+
+function createStartMonth () {
+  return getStartOfYear().toFormat('yyyyMM')
+}
+
+function getEndDMonthFormElement () {
+  return document.getElementById('cboYM2')
+}
+
+function createEndDMonth () {
+  return getNow().toFormat('yyyyMM')
+}
