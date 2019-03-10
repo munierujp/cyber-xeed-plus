3996b991eb1d0960776ca66c8035e5fa6cbd3dfa
Munieru <20086673+munierujp@users.noreply.github.com>
2019-03-10 09:48:21 +0900

    Add 就業週報月報画面

--- app/scripts/modules/paths.js
+++ app/scripts/modules/paths.js
@@ -1,6 +1,7 @@
 export default {
   ログイン画面: '/cyberx/login.asp',
   勤務データ確認画面: '/cyberx/page/Xyw1100g.asp',
+  就業週報月報画面: '/cyberx/page/Xaw1500.asp',
   就業週報画面: '/cyberx/page/Xaw1800.asp',
   就業月報画面: '/cyberx/page/Xyw3300.asp'
 }
